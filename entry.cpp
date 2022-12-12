538b744d32f41a26aaf76889baad87cf178976c7
eftw <84478811+eftw@users.noreply.github.com>
2022-12-12 03:39:28 +0000

    Create entry.cpp

---
+++ entry.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() // credits to exprssn for helping <3
+{
+	unsigned int years{};
+	float days{};
+
+	std::printf("How old are you? ");
+	std::cin >> years;
+
+	std::printf("How many days has it been since your birthday? ");
+	std::cin >> days;
+
+	float age = (days / 365) + years;
+
+	std::cout << "You are " << age << " years old.\n";
+
+	std::cin.get();
+	return 0;
+}
